d9ee44a43c14c15884bd2f59ea9648819830c2de
Oluwatimilehin Alo <timisnup@gmail.com>
2022-08-15 00:50:37 +0100

    first commit

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,8 @@
 resource "aws_vpc" "timmie-vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
+  enable_dns_hostnames = true
+  enable_dns_support = true
 
   tags = {
     Name = "timmie-vpc"
