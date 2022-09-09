53400e5b1bf882de47be2441ac642937fe0bafb8
Oluwatimilehin Alo <timisnup@gmail.com>
2022-09-09 03:17:40 +0100

    updated sg

--- secgroup.tf
+++ secgroup.tf
@@ -23,6 +23,15 @@ resource "aws_security_group" "secgrp" {
   }
 
   ingress {
+    description = "https"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
     description = "ssh"
     from_port   = 22
     to_port     = 22
